4f4804e728e20b6e11e09d54d0fbfb6e5a54df54
Misha <mihailavetisan92@gmail.com>
2026-03-03 22:46:20 +0200

    03.03.26 First finish, waiting for rejects

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import defs
-from defs import first_player_move
 another_round: str = ""
 _table: list = [str]
 row_choice: str = ""
@@ -9,27 +8,13 @@ move: str = ""
 current_player: str = ""
 first_player_score: int = 0
 second_player_score: int = 0
-# # 1) надо сделать главный экран
-# 1.1) принт текста с декоративными полосками и кнопкой запуска игры
+
 print("-" * 30)
 print("Welcome to the great TIC TAC TOE tournament!")
 print("❌ ⚔️ ⭕")
 print("-" * 30)
 input("To continue, press any key to continue")
-# # 2)нужно сделать табличку 3х3
-# 2.1) сделаю список
-# # 3)ходы игроков
-# 3.1) инпут требует номер строки и столба
-# 3.2) проверка инпута
-# 3.3) внесение в таблицу и печать
 
-# 3.4) проверка на выигрыш
-# 3.5) ход следующего игрока
-
-
-# # 4)результат
-# 4.1) если было найдено 3 подряд - представить таблицу с обведённой тройкой
-# 4.2) спросить продолжать ли ещё
 while True:
     _table = [
         ["⬜", "⬜", "⬜"],
